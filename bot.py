f2bf7aba4716c1eed4ffe4593985b6cebf900d85
minupower-ship-it <minupower@gmail.com>
2025-12-13 11:46:11 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,50 +1,46 @@
 import tweepy
+import os
 import time
 
-# --- API 키 / 토큰 ---
-API_KEY = "bT0MePTguMvAurdPxx37AzbVq"
-API_SECRET = "qN42QJoglnR7Go5J1nGSTh23o9lCyFU3kbzEDhsvjgtXYMqEC0"
-ACCESS_TOKEN = "1983673230792765441-zu5NopX4PI8Ar1PUCPdmWiSgiXTvPP"
-ACCESS_TOKEN_SECRET = "3yHndFORNxhUTfVHwIE8ABPiMjcDe7vAVes5MEdb1GxLJ"
+# --- 환경변수에서 API 키 불러오기 ---
+API_KEY = os.environ["API_KEY"]
+API_SECRET = os.environ["API_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 
-# --- 보낼 메시지 ---
+# --- 전송할 메시지 ---
 MESSAGE = (
     "DM for more vids\n"
     "Video: https://files.catbox.moe/em10m4.mp4\n"
     "Telegram bot: http://t.me/obtkryptobot"
 )
 
-# --- Tweepy 클라이언트 생성 ---
-client = tweepy.Client(
-    consumer_key=API_KEY,
-    consumer_secret=API_SECRET,
-    access_token=ACCESS_TOKEN,
-    access_token_secret=ACCESS_TOKEN_SECRET
-)
+# --- Tweepy v1.1 인증 ---
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
-me = client.get_me().data.id
-last_dm_id = None
+# 내 계정 ID 확인
+MY_ID = api.me().id_str
 replied_users = set()  # 유저당 1회만 전송
 
 print("봇 실행중...")
 
 while True:
-    dms = client.get_direct_messages()
-
-    if dms.data:
-        for dm in reversed(dms.data):
-            sender = dm.sender_id
+    try:
+        # 최근 20개 DM 가져오기
+        dms = api.get_direct_messages(count=20)
+        for dm in reversed(dms):
+            sender = dm.message_create['sender_id']
 
             # 본인 제외 + 1회만 전송
-            if sender != me and sender not in replied_users:
-                client.send_direct_message(
-                    recipient_id=sender,
-                    text=MESSAGE
-                )
-                replied_users.add(sender)
-                print("DM 전송 완료:", sender)
-
-            # 마지막 DM ID 갱신
-            last_dm_id = dm.id
-
-    time.sleep(10)  # 10초마다 확인
+            if sender != MY_ID and sender not in replied_users:
+                try:
+                    api.send_direct_message(recipient_id=sender, text=MESSAGE)
+                    replied_users.add(sender)
+                    print("DM 전송 완료:", sender)
+                except Exception as e:
+                    print("DM 전송 실패:", e)
+        time.sleep(10)
+    except Exception as e:
+        print("DM 가져오기 실패:", e)
+        time.sleep(30)
